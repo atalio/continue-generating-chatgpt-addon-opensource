4603ea59018cb49603ef372f56445ca89021f90a
cristóbal <36176343+criskkky@users.noreply.github.com>
2023-06-02 03:25:11 -0400

    Update v2.3
    + A visual notification has been added in the top right corner to indicate both the activation of the extension and its actions.
    + The way the automatic click on the "Continue generating" button is executed has been changed. It no longer consumes unnecessary resources by checking every 1 second if the button is present. Now, it detects the addition of new elements, and whenever this happens, it will search for the button.

--- Continue-Generating-for-ChatGPT/content.js
+++ Continue-Generating-for-ChatGPT/content.js
@@ -1,4 +1,5 @@
-/*
+/* Version: 2.3 |
+-----------------
 Author: @criskkky
 Website: https://criskkky.carrd.co/
 Extension URL: https://addons.mozilla.org/es/firefox/addon/continuegenerating-chatgpt/
@@ -7,16 +8,77 @@ Please rate 5 stars if it's useful <3
 */
 
 (function () {
-  function clickContinueButton() {
-    const buttons = document.querySelectorAll(".btn");
+  const startUp = 1;
+  const body = document.body;
 
-    for (let i = 0; i < buttons.length; i++) {
-      if (buttons[i].innerText.includes("Continue generating")) {
-        buttons[i].click();
-        break;
-      }
-    }
+  const iconURL = "https://raw.githubusercontent.com/criskkky/continue-generating-chatgpt-addon-opensource/main/src/rgpt.svg";
+
+  function showNotification() {
+    const notification = document.createElement("div");
+    const iconLink = document.createElement("a");
+    const icon = document.createElement("img");
+    icon.src = iconURL;
+    icon.style.cssText = "width: 30px; height: 30px";
+    iconLink.href = "https://addons.mozilla.org/es/firefox/addon/continuegenerating-chatgpt/";
+    iconLink.target = "_blank";
+    iconLink.title = "Continue Generating Auto-Clicker";
+    iconLink.appendChild(icon);
+    notification.appendChild(iconLink);
+
+    notification.style.cssText = "position: fixed; top: 0px; right: 5px; align-items: center; padding: 10px; z-index: 9999;";
+    body.appendChild(notification);
+
+    notification.animate(
+      [
+        { opacity: 1, transform: 'translateY(-60px)'},
+        { opacity: 1, transform: 'translateY(0)'},
+      ],
+      { duration: 600, easing: "ease-in-out" }
+    ).onfinish = () => {
+      icon.animate(
+        [
+          { transform: 'rotate(0deg)' },
+          { transform: 'rotate(-360deg)' },
+        ],
+        { duration: 500, easing: "linear", iterations: Infinity }
+      );
+    };
+
+    setTimeout(() => {
+      notification.animate(
+        [
+          { opacity: 1, transform: 'translateY(0) rotate(-360deg)' },
+          { opacity: 1, transform: 'translateY(-60px) rotate(-360deg)' },
+        ],
+        { duration: 1000, easing: "ease-in-out" }
+      ).onfinish = () => {
+        notification.remove();
+      };
+    }, 2000);
   }
 
-  setInterval(clickContinueButton, 1000);
+  function clickContinueButton() {
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+          mutation.addedNodes.forEach((node) => {
+            if (node.nodeType === Node.ELEMENT_NODE && node.innerText.includes("Continue generating")) {
+              node.click();
+              if (startUp) {
+                showNotification();
+              }
+            }
+          });
+        }
+      });
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+  }
+
+  if (startUp) {
+    showNotification();
+  }
+
+  clickContinueButton();
 })();
